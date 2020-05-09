868a79141431d8ae01cd3ff4fecd75f1307736c1
Pepe Iborra <pepeiborra@gmail.com>
2020-05-09 21:24:35 +0100

    bump all-cabal-hashes

--- overlays/haskell/data/hackage.nix
+++ overlays/haskell/data/hackage.nix
@@ -1,4 +1,4 @@
 builtins.fetchurl {
-              url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/19e6cb11d42b9b1ff01829aa80c867e98ba57f96.tar.gz";
-              sha256 = "149642srsw6w31qa3sr10f2khc62cn2p50hclh1dk0h0ffyllhy8";
+              url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/ca3d0b2c9af84fbb997c286ecc63b757fb2c549e.tar.gz";
+              sha256 = "0dadbrdzn8w8fp2jm9caiyn7abln4mkq0l4iqsmwbkai6lwcfsv1";
               }
