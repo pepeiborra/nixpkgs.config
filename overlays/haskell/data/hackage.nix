c0c35ed3d0780312cede7f422e1706cc78cfc5c6
Pepe Iborra <pepeiborra@gmail.com>
2020-05-17 14:15:38 +0100

    bump Cabal hashes

--- overlays/haskell/data/hackage.nix
+++ overlays/haskell/data/hackage.nix
@@ -1,4 +1,4 @@
 builtins.fetchurl {
-              url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/ca3d0b2c9af84fbb997c286ecc63b757fb2c549e.tar.gz";
-              sha256 = "0dadbrdzn8w8fp2jm9caiyn7abln4mkq0l4iqsmwbkai6lwcfsv1";
+              url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/5345723cbea7e315f40b18bf8f3e8ff248439946.tar.gz";
+              sha256 = "0mn3wbvarh1hvhkz025nc0gvws9c09jz5jl8qaw6b0b50i647x4q";
               }
